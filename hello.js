1b3ded4da6f7a1e836fbec14eb95afc81c3bdbd6
Benoît Hubert <benoit.hubert@wildcodeschool.com>
2020-10-05 13:53:50 +0200

    Initial commit (load js with different script tags)

---
+++ hello.js
@@ -0,0 +1,5 @@
+'use strict'
+
+function hello() {
+  document.body.innerHTML = '<h1>Hello World</h1>';
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+// Fichier principal de l' application
+
+hello();
+
+name = 'Ada';
+console.log(name);
